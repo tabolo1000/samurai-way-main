ff6324a41aedf0061c95ef5f041120a424d76b26
Nik <tabola1000@gmail.com>
2023-11-17 16:08:41 +0100

    add life circle componentDidMount for users componenet make pure function

--- src/components/Navbar/Users/Users.tsx
+++ src/components/Navbar/Users/Users.tsx
@@ -26,14 +26,13 @@ interface photosType {
 }
 
 export class Users extends React.Component<propsType> {
-    constructor(props: propsType) {
-        debugger
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then((response: any) => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             })
     }
+
     render() {
 
         debugger
